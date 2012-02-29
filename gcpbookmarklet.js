a6a3675aa7d610019c345b926cba28a27676e67b
Peter Camfield <petercamfield@gmail.com>
2012-02-29 23:09:08 +0000

    'Updated web page'

--- gcpbookmarklet.js
+++ gcpbookmarklet.js
@@ -1,16 +1,18 @@
-var script=document.createElement('SCRIPT');
-script.src='http://www.google.com/cloudprint/client/cpgadget.js';
-script.type='text/javascript';
-document.body.appendChild(script);
+(function(){
+    var script=document.createElement('SCRIPT');
+    script.src='http://www.google.com/cloudprint/client/cpgadget.js';
+    script.type='text/javascript';
+    document.body.appendChild(script);
 
-var printDiv=document.createElement('DIV');
-printDiv.id='bookmarklet_print_button_container';
-document.body.appendChild(printDiv);
+    var printDiv=document.createElement('DIV');
+    printDiv.id='bookmarklet_print_button_container';
+    document.body.appendChild(printDiv);
 
-var addPrintButton = function() {
-    var gadget = new cloudprint.Gadget();
-	gadget.setPrintButton(cloudprint.Gadget.createDefaultPrintButton("bookmarklet_print_button_container"));
-	gadget.setPrintDocument("url", document.title, document.URL);
-};
+    var addPrintButton = function() {
+        var gadget = new cloudprint.Gadget();
+	    gadget.setPrintButton(cloudprint.Gadget.createDefaultPrintButton("bookmarklet_print_button_container"));
+	    gadget.setPrintDocument("url", document.title, document.URL);
+    };
 
-window.setTimeout(addPrintButton, 200);
+    window.setTimeout(addPrintButton, 200);
+})();
